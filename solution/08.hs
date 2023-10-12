6d5cdcf83b52cfe10329d8a1643463dd6b95465e
Emil Overbeck <emil.a.overbeck@gmail.com>
2023-10-12 23:24:31 +0200

    Slightly simplify 08

--- solution/08.hs
+++ solution/08.hs
@@ -1,7 +1,5 @@
 import Lib
 
-import Debug.Trace (traceShowId)
-
 type Tree = Int     -- represents height
 
 surrounding :: (Int, Int) -> [[Tree]] -> [[Tree]]
@@ -9,6 +7,7 @@ surrounding (y, x) ts = [reverse l, r, reverse u, d]
   where (l, r) = pierceAt x (ts !! y)
         (u, d) = pierceAt y [ts !! y' !! x | y' <- [0 .. length ts - 1]]
 
+innerIxs :: [[Tree]] -> [(Int, Int)]
 innerIxs ts = [(y, x) | y <- [1 .. length ts - 2],
                         x <- [1 .. length (head ts) - 2]]
 
@@ -22,20 +21,19 @@ countVisible ts = innerIxs ts
     & (+ numEdge)
   where isVisible (y, x) = let t = ts !! y !! x in
              not . all (any (>= t)) $ surrounding (y, x) ts
-
         numEdge = length ts * 2 + (length (head ts) - 2) * 2
 
--- we assume the highest score won't be at the edges
+-- we assume the highest score isn't at the edges
 maxScenicScore :: [[Tree]] -> Int
 maxScenicScore ts = maximum $ map scenicScore (innerIxs ts)
   where scenicScore (y, x) = let t = ts !! y !! x in
             surrounding (y, x) ts
                 & map (length . takeWhileInc (< t))
                 & product
-        takeWhileInc p [] = []
-        takeWhileInc f (x : xs) = if f x
-           then x : takeWhileInc f xs
-           else [x]
+        takeWhileInc _ [] = []
+        takeWhileInc f (x : xs) =
+            if f x then x : takeWhileInc f xs
+                   else [x]
 
 main = do
     trees <- parseTrees <$> readInput 8
